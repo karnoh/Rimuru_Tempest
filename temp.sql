3eee429e2c6028c3258daf8587e340ff22a64b49
karnoh <113664695+karnoh@users.noreply.github.com>
2023-10-26 11:17:24 +0530

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -1,28 +1,247 @@
-SELECT
-  'test1' AS sample_no,
-  dbmon.db_name,
-  dbmon.db_status,
-  dbmon.db_conn_time,
-  dbmon.db_act_time,
-  dbmon.db_act_aborted,
-  dbmon.db_act_completed,
-  dbmon.db_act_rejected,
-  dbmon.db_rows_read,
-  dbmon.db_rows_written,
-  bp.bp_name,
-  bp.bp_id,
-  bp.bp_cur_pages,
-  bp.bp_async_data_read_reqs,
-  bp.bp_async_data_write_reqs,
-  bp.bp_async_index_read_reqs,
-  bp.bp_async_index_write_reqs,
-  bp.bp_direct_reads,
-  bp.bp_direct_writes,
-  bp.bp_direct_read_reqs,
-  bp.bp_direct_write_reqs
-FROM
-  TABLE(MON_GET_DATABASE(-2)) AS dbmon
-  CROSS JOIN
-  LATERAL (SELECT * FROM TABLE(MON_GET_TABLE('SYSIBMADM', 'SNAPDB', -2))) AS db
-  CROSS JOIN
-  LATERAL (SELECT * FROM TABLE(MON_GET_BUFFERPOOL(-2))) AS bp
+--
+-- BLOB and CLOB
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TABSCHEMA) CONCAT ',' CONCAT
+	
+	v.typename CONCAT ',' CONCAT
+	
+	COALESCE(count(v.typename), 1) CONCAT ',1,1'		
+
+from syscat.columns as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+where v.typename in ('BLOB', 'CLOB')
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.TABSCHEMA, v.typename;
+
+
+--
+-- TABLE PARTITIONS
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TABSCHEMA) CONCAT ',' CONCAT	
+	'TABLE PARTITION' CONCAT ',' CONCAT	
+	COALESCE(count(v.TABNAME), 1) CONCAT ',1,1'		
+
+from syscat.TABDETACHEDDEP as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.TABSCHEMA;
+
+
+--
+-- ALIASES
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TABSCHEMA) CONCAT ',' CONCAT	
+	'ALIAS' CONCAT ',' CONCAT	
+	COALESCE(count(v.TABNAME), 1) CONCAT ',1,1'		
+
+from syscat.tables as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+where v.type = 'A'
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.TABSCHEMA;
+
+
+--
+-- MATERIALIZED QUERY TABLES
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TABSCHEMA) CONCAT ',' CONCAT	
+	'MATERIALIZED QUERY TABLE' CONCAT ',' CONCAT	
+	COALESCE(count(v.TABNAME), 1) CONCAT ',1,1'		
+
+from syscat.tables as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+where v.type = 'S'
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.TABSCHEMA;
+
+
+--
+-- INDEXES
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.INDSCHEMA) CONCAT ',' CONCAT	
+	'INDEX' CONCAT ',' CONCAT	
+	COALESCE(count(v.INDNAME), 1) CONCAT ',1,1'		
+
+from syscat.indexes as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.INDSCHEMA;
+
+
+--
+-- SEQUENCES
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.SEQSCHEMA) CONCAT ',' CONCAT
+	
+	'SEQUENCE' CONCAT ',' CONCAT
+	
+	COALESCE(count(v.SEQNAME), 1) CONCAT ',1,1'		
+
+from syscat.sequences as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.SEQSCHEMA;
+
+
+--
+-- TRIGGERS
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TRIGSCHEMA) CONCAT ',' CONCAT
+	
+	'TRIGGER' CONCAT ',' CONCAT
+	
+	COALESCE(count(v.TRIGNAME), 1) CONCAT ',' CONCAT
+	COALESCE(sum(length(v.TEXT)), 1) CONCAT ',' CONCAT
+	COALESCE((sum(length(v.TEXT)) / count(v.TRIGNAME)), 1)	
+
+from syscat.triggers as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.TRIGSCHEMA;
+
+
+--
+--  VIEWS
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.VIEWSCHEMA) CONCAT ',' CONCAT
+	
+	'VIEW' CONCAT ',' CONCAT
+	
+	COALESCE(count(v.VIEWNAME), 1) CONCAT ',' CONCAT
+	COALESCE(sum(length(v.TEXT)), 1) CONCAT ',' CONCAT
+	COALESCE((sum(length(v.TEXT)) / count(v.VIEWNAME)), 1)	
+
+from syscat.views as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.VIEWSCHEMA;
+
+
+--
+--  PROCEDURES, FUNCTIONS, METHODS
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT	
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.ROUTINESCHEMA) CONCAT ',' CONCAT
+
+	case when v.ROUTINETYPE = 'P' then 'PROCEDURE'
+		 when v.ROUTINETYPE = 'F' then 'FUNCTION'
+		 when v.ROUTINETYPE = 'M' then 'METHOD'
+	end CONCAT ',' CONCAT
+
+	COALESCE(count(v.ROUTINENAME), 1) CONCAT ',' CONCAT
+	COALESCE(sum(length(v.TEXT)), 1) CONCAT ',' CONCAT
+	COALESCE((sum(length(v.TEXT)) / count(v.ROUTINENAME)), 1)	
+
+from syscat.routines as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.ROUTINESCHEMA, v.ROUTINETYPE;
+
+
+
+
+
+
+
+
+
+
+
+
+--
+-- RAW DATA
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TABSCHEMA) CONCAT ',,' CONCAT
+	
+	'RAW DATA (MB)' CONCAT ',' CONCAT
+	
+	round((sum(v.CARD * v.AVGROWSIZE) * 0.000001), 2)
+	
+from syscat.tables as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+group by e.OS_NAME, i.SERVICE_LEVEL, i.INST_NAME, e.HOST_NAME, v.TABSCHEMA;
+
+
+--
+-- TABLE SIZING
+--
+
+select
+
+	trim(e.OS_NAME) CONCAT ',' CONCAT
+	trim(e.HOST_NAME) CONCAT ',' CONCAT
+	trim(i.INST_NAME) CONCAT ',' CONCAT
+	trim(i.SERVICE_LEVEL) CONCAT ',' CONCAT
+	trim((SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1)) CONCAT ',' CONCAT
+	trim(v.TABSCHEMA) CONCAT ',' CONCAT	
+	v.TABNAME CONCAT ',' CONCAT
+	
+	'TABLE SIZING' CONCAT ',' CONCAT
+	
+	v.CARD CONCAT ',' CONCAT
+	v.AVGROWSIZE CONCAT ',' CONCAT
+	(v.CARD * v.AVGROWSIZE)
+	
+	
+from syscat.tables as v, TABLE(SYSPROC.ENV_GET_SYS_INFO()) as e, TABLE(SYSPROC.ENV_GET_INST_INFO()) as i
+where v.type = 'T';
