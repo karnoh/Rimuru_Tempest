42ead471ad9bf04b8deab198ce36e915a4e23c42
karnoh <113664695+karnoh@users.noreply.github.com>
2023-10-27 12:48:00 +0530

    Update temp001.sql

--- temp001.sql
+++ temp001.sql
@@ -1,141 +1,65 @@
-SELECT 'Database Health Report' AS "Category", '' AS "Metric", '' AS "Value"
-UNION ALL
+WITH QueryPerformanceData AS (
+    SELECT
+        APPLICATION_HANDLE AS ApplicationHandle,
+        STMT_TEXT AS QueryText,
+        TOTAL_EXEC_TIME AS TotalExecutionTime,
+        EXECUTION_TIME AS CPUTime,
+        BUFFER_POOL_DATA_L_READS AS NumberOfRowsReturned,
+        LOCK_WAIT_TIME AS LockWaitTime,
+        SORTS_TOTAL_TIME AS SortTime,
+        TOTAL_APP_COMMITS AS Commits,
+        TOTAL_APP_ROLLBACKS AS Rollbacks,
+        IO_TIME AS IOWaitTime
+    FROM
+        TABLE(MON_GET_ACTIVITY(-1, NULL)) AS MON
+    WHERE
+        STMT_TEXT IS NOT NULL
+        AND EXECUTION_TIME > 0
+)
 
--- Query Performance
-SELECT 'Query Performance' AS "Category",
-       'Number of Queries' AS "Metric",
-       COUNT(*) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Query Performance' AS "Category",
-       'Total Execution Time (ms)' AS "Metric",
-       SUM(TOTAL_EXEC_TIME_MS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Query Performance' AS "Category",
-       'Average Execution Time (ms)' AS "Metric",
-       AVG(TOTAL_EXEC_TIME_MS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Query Performance' AS "Category",
-       'Max Execution Time (ms)' AS "Metric",
-       MAX(TOTAL_EXEC_TIME_MS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-
-UNION ALL
-
--- Transaction Statistics
-SELECT 'Transaction Statistics' AS "Category",
-       'Number of Transactions' AS "Metric",
-       COUNT(*) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Transaction Statistics' AS "Category",
-       'Total Commits' AS "Metric",
-       SUM(COMMIT_SQL_STMTS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Transaction Statistics' AS "Category",
-       'Total Rollbacks' AS "Metric",
-       SUM(ROLLBACK_SQL_STMTS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-
-UNION ALL
-
--- Locking and Blocking
-SELECT 'Locking and Blocking' AS "Category",
-       'Total Deadlocks' AS "Metric",
-       SUM(TOTAL_DEADLOCKS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Locking and Blocking' AS "Category",
-       'Total Lock Timeouts' AS "Metric",
-       SUM(TOTAL_LOCK_TIMEOUTS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Locking and Blocking' AS "Category",
-       'Total Lock Escalations' AS "Metric",
-       SUM(TOTAL_LOCK_ESCALATIONS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-
-UNION ALL
-
--- Buffer Pool Usage
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Data Logical Reads' AS "Metric",
-       SUM(POOL_DATA_L_READS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Index Logical Reads' AS "Metric",
-       SUM(POOL_INDEX_L_READS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Data Physical Reads' AS "Metric",
-       SUM(POOL_DATA_P_READS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Index Physical Reads' AS "Metric",
-       SUM(POOL_INDEX_P_READS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Async Data Reads' AS "Metric",
-       SUM(POOL_ASYNC_DATA_READS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Async Index Reads' AS "Metric",
-       SUM(POOL_ASYNC_INDEX_READS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Async Data Writes' AS "Metric",
-       SUM(POOL_ASYNC_DATA_WRITES) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Buffer Pool Usage' AS "Category",
-       'Async Index Writes' AS "Metric",
-       SUM(POOL_ASYNC_INDEX_WRITES) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-
-UNION ALL
-
--- Sort Activity
-SELECT 'Sort Activity' AS "Category",
-       'Total Sorts' AS "Metric",
-       SUM(TOTAL_SORTS) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Sort Activity' AS "Category",
-       'Total Sort Time (ms)' AS "Metric",
-       SUM(TOTAL_SORT_TIME) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-
-UNION ALL
-
--- Application Connections
-SELECT 'Application Connections' AS "Category",
-       'Number of Active Connections' AS "Metric",
-       COUNT(DISTINCT APPL_ID) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-
-UNION ALL
-
--- Resource Utilization
-SELECT 'Resource Utilization' AS "Category",
-       'Average Statement Execution Time (s)' AS "Metric",
-       DECIMAL(AVG_STMT_EXEC_TIME / 1000, 10, 2) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Resource Utilization' AS "Category",
-       'Average Lock Wait Time (s)' AS "Metric",
-       DECIMAL(AVG_LOCK_WAIT_TIME / 1000, 10, 2) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T
-UNION ALL
-SELECT 'Resource Utilization' AS "Category",
-       'Max Agent CPU Time (s)' AS "Metric",
-       DECIMAL(MAX_AGENT_CPU_TIME / 1000, 10, 2) AS "Value"
-FROM TABLE (SYSPROC.MON_GET_ACTIVITY(NULL, -2)) AS T;
+SELECT
+    *,
+    (CASE
+        WHEN TotalExecutionTime < 1000 THEN 0
+        WHEN TotalExecutionTime BETWEEN 1000 AND 5000 THEN 1
+        WHEN TotalExecutionTime BETWEEN 5000 AND 10000 THEN 2
+        ELSE 3
+    END
+    +
+    CASE
+        WHEN CPUTime < 1000 THEN 0
+        WHEN CPUTime BETWEEN 1000 AND 5000 THEN 1
+        WHEN CPUTime BETWEEN 5000 AND 10000 THEN 2
+        ELSE 3
+    END
+    +
+    (CASE
+        WHEN MemoryUsage < 1000 THEN 0
+        WHEN MemoryUsage BETWEEN 1000 AND 10000 THEN 1
+        WHEN MemoryUsage BETWEEN 10000 AND 100000 THEN 2
+        ELSE 3
+    END)
+    +
+    (CASE
+        WHEN NumberOfRowsReturned < 100 THEN 0
+        WHEN NumberOfRowsReturned BETWEEN 100 AND 1000 THEN 1
+        WHEN NumberOfRowsReturned BETWEEN 1000 AND 10000 THEN 2
+        ELSE 3
+    END
+    +
+    (CASE
+        WHEN LockWaitTime < 1000 THEN 0
+        WHEN LockWaitTime BETWEEN 1000 AND 5000 THEN 1
+        WHEN LockWaitTime BETWEEN 5000 AND 10000 THEN 2
+        ELSE 3
+    END
+    +
+    (CASE
+        WHEN IOWaitTime < 1000 THEN 0
+        WHEN IOWaitTime BETWEEN 1000 AND 5000 THEN 1
+        WHEN IOWaitTime BETWEEN 5000 AND 10000 THEN 2
+        ELSE 3
+    END)
+    ) AS TotalScore
+FROM
+    QueryPerformanceData;
